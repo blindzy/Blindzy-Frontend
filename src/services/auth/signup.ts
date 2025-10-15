0b4e86a7d378131fec438eb3329ddde31eaf8e44
usama baloch <usamabaloch0005@gmail.com>
2025-10-15 06:36:06 +0500

    chdsa

--- src/services/auth/signup.ts
+++ src/services/auth/signup.ts
@@ -1,6 +1,6 @@
 export class SignupService {
   // private baseUrl = import.meta.env.VITE_API_URL;
-  private baseUrl = import.meta.env.VITE_API_URL || "http://208.87.135.120:9000"; // Medusa backend URL
+  private baseUrl = import.meta.env.VITE_API_URL || "https://208.87.135.120:9000"; // Medusa backend URL
   private baseUrl_KEY = import.meta.env.VITE_MEDUSA_PUBLISHABLE_KEY || "pk_618e1efe2b7d74576d7e072b76bd3c56d46ef94ede9463774e1c2b09c50f6bda"; // Medusa backend URL
 
   private async medusaRequest<T>(endpoint: string, options: RequestInit): Promise<T> {
--- src/services/store-cardDetail.ts
+++ src/services/store-cardDetail.ts
@@ -1,5 +1,5 @@
 export class StoreCardDetails {
-  private baseUrl = import.meta.env.VITE_API_URL || "http://208.87.135.120:9000"; // Medusa backend URL
+  private baseUrl = import.meta.env.VITE_API_URL || "https://208.87.135.120:9000"; // Medusa backend URL
   private baseUrl_KEY = import.meta.env.VITE_MEDUSA_PUBLISHABLE_KEY || "pk_618e1efe2b7d74576d7e072b76bd3c56d46ef94ede9463774e1c2b09c50f6bda"; // Medusa backend URL
 
   private async medusaRequest<T>(endpoint: string, options: RequestInit): Promise<T> {
